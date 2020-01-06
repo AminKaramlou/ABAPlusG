00ecf47ef9285ac2bea9e9ec7a22eeee3e24f321
Kristijonas Čyras <k.cyras@imperial.ac.uk>
2020-01-06 17:39:00 +0000

    counter-examples to flat ABA+ frameworks (satisfying WCP) with reverse attacks and with and without facts

---
+++ test_reverse_fact_flat_WCP.pl
@@ -0,0 +1,16 @@
+myAsm(a).
+myAsm(b).
+myAsm(c).
+myAsm(d).
+myAsm(e).
+contrary(a, C(a)).
+contrary(b, C(b)).
+contrary(c, C(c)).
+contrary(d, C(d)).
+contrary(e, C(e)).
+myRule(C(b), [c, d]).
+myRule(C(c), [a, d]).
+myRule(C(a), [c, d]).
+myRule(C(d), [d]).
+myRule(C(e), []).
+myPrefLT(c, a).
