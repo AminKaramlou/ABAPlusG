8ada773656c6a8189ea921677dcb5fb7c9eee126
Kristijonas Čyras <k.cyras@imperial.ac.uk>
2020-01-06 16:45:45 +0000

    added CLI for computing extensions of an ABA+ framework

--- src/cli.py
+++ src/cli.py
@@ -3,10 +3,13 @@ import json
 from src.aba_plus_g_parse import generate_aba_plus_g_framework_from_file
 from src.explanations import get_explanations_json
 from src.tmr_to_aba_plus_g import transform_dss_input_to_aba_plus_file
+from src.labelling_algorithms import construct_grounded_labelling
 
 @click.command()
 @click.option('--file',
               help='Path to file defining the DSS json data.')
+
+
 def generate_explanations(file):
     with open(file, 'r') as f:
         data = json.load(f)
@@ -20,3 +23,16 @@ def generate_explanations(file):
     click.echo(explanation)
     with open(framework_file_name + '.json', 'w') as file:
         file.write(explanation)
+
+
+def compute_extensions(file):
+    abapf = generate_aba_plus_g_framework_from_file(file)
+    extensions = get_extensions(abapf)
+    click.echo("--------------------------Extensions-----------------------")
+    click.echo(extensions)
+    click.echo("--------------------------Extensions-----------------------")
+    dump_extensions(extensions, file)
+
+
+if __name__ == "__main__":
+    compute_extensions()
